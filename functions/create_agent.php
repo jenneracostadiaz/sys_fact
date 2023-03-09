ca1c36deeea263dde96503739b601334c0380564
Jenner Acosta Diaz <jenner.acosta@itware.com.pe>
2023-03-08 23:21:03 -0500

    Add New User: Cotain Error Line 65 create_agente.php

--- functions/create_agent.php
+++ functions/create_agent.php
@@ -3,21 +3,78 @@
     require __DIR__ . '/uris.php';
     require __DIR__ . '/redirect.php';
 
+    $s_company = $_POST['s_company'];
     $username = $_POST['username'];
     $fullname = $_POST['fullname'];
     $phone = $_POST['phone'];
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    echo '<pre>';
-    var_dump($username);
-var_dump($fullname);
-var_dump($phone);
-var_dump($email);
-var_dump($password);
-    echo '</pre>';
+    //Valide Company
+    $consult = "SELECT * FROM companies WHERE CompanieID=$s_company";
+    $result = mysqli_query($connection, $consult);
+    if(mysqli_num_rows($result) == 0){
+        $redirect_to = sys_domain().'/new-agent.php?company=novalid';
+        sys_redirect( $redirect_to );
+        exit;
+    }
     
+    //Valide Data User
+    if (!preg_match('/^[a-zA-Z0-9]{3,15}$/', $username)) {
+        $redirect_to = sys_domain().'/new-agent.php?username=novalid';
+        sys_redirect( $redirect_to );
+        exit;  
+    }
+    if (!preg_match('/^[a-zA-ZáéíóúÁÉÍÓÚäëïöüÄËÏÖÜàèìòùÀÈÌÒÙ\s\.]+$/', $fullname)) {
+        $redirect_to = sys_domain().'/new-agent.php?fullname=novalid';
+        sys_redirect( $redirect_to );
+        exit;  
+    }
+    if (!preg_match('/^[0-9]{9,13}$/', $phone)) {
+        $redirect_to = sys_domain().'/new-agent.php?phone=novalid';
+        sys_redirect( $redirect_to );
+        exit;  
+    }
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $redirect_to = sys_domain().'/new-agent.php??email=novalid';
+        sys_redirect( $redirect_to );
+        exit;   
+    }
+
+    $stmt = $connection->prepare("INSERT INTO users (Username, Name, Email, Phone, Password) VALUES (?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssss", $username, $fullname, $phone, $email, $password);
+    $stmt->execute();
+
+    if (!$stmt->execute()) { 
+        $redirect_to = sys_domain().'/new-agent.php?insert=error';
+        sys_redirect( $redirect_to );
+        exit;
+     }
     
+    $consult = "SELECT ID FROM users WHERE Username=$username";
+    $result = mysqli_query($connection, $consult);
+    if ($result->num_rows > 0){
+        while($row = $result->fetch_assoc()){
+            $UserID = $row["UserID"];
+        }
+    }
+
+    $rolID = '3';
+
+    $r_stmt = $connection->prepare("INSERT INTO systrelations (UserID, RolID, CompanieID) VALUES (?, ?, ?)");
+    $r_stmt->bind_param("iii", $UserID, $rolID, $s_company);
+    $r_stmt->execute();
+
+    if (!$r_stmt->execute()) { 
+        $redirect_to = sys_domain().'/new-agent.php?insert=error&row=2';
+        sys_redirect( $redirect_to );
+        exit;
+     } else {
+        $redirect_to = sys_domain().'/new-agent.php?insert=successfully';
+        sys_redirect( $redirect_to );
+        exit;
+     }
     
     
 ?>
--- layouts/pg_newAgent.php
+++ layouts/pg_newAgent.php
@@ -1,8 +1,45 @@
 <?php require_once __DIR__.'/ly_header.php'; ?>
 <?php require_once __DIR__.'/ly_nav.php'; ?>
+<?php 
+    if(!empty($_GET['company'])){
+        $company = $_GET['company'];
+    }
+    if(!empty($_GET['username'])){
+        $username = $_GET['username'];
+    }
+    if(!empty($_GET['fullname'])){
+        $fullname = $_GET['fullname'];
+    }
+    if(!empty($_GET['phone'])){
+        $phone = $_GET['phone'];
+    }
+    if(!empty($_GET['email'])){
+        $email = $_GET['email'];
+    }
+    if(!empty($_GET['database'])){
+        $database = $_GET['database'];
+    }
+    if(!empty($_GET['insert'])){
+        $insert = $_GET['insert'];
+    }
+?>
 
 <main class="container new-agent">
     <h1>Crear un nuevo Representante</h1>
+    <?php 
+        if(!empty($_GET['database']) && $database == 'erroconex'){
+            echo '<div class="alert error">Error en la conexión con la Base de Datos</div>';
+        }
+        if(!empty($_GET['database']) && $database == 'userexist'){
+            echo '<div class="alert error">El Nombre de Usuario ya existe en la base de datos</div>';
+        }
+        if(!empty($_GET['insert']) && $insert == 'error'){
+            echo '<div class="alert error">Tuvimos un error al insertar los datos, vuelve a intentarlo más tarde</div>';
+        }
+        if(!empty($_GET['insert']) && $insert == 'successfully'){
+            echo '<div class="alert success">Felicidades! insertaste una nueva empresa. Relacionalo con un representante para que empiece a funcionar en el sistema.</div>';
+        }
+    ?>
     <form action="<?php print(sys_domain()).'/functions/create_agent.php'; ?>" class="form" method="post">
         <?php
             $result = connectDB()->query(query_companies());
@@ -18,23 +55,47 @@
                     echo '</select>';
                 echo '</div>';
             }
+            if(!empty($_GET['company']) && $company == 'novalid'){
+                echo '<div class="alert error">Empresa no encontrada</div>';
+            }
         ?>
+        
         <div class="form__group">
             <label for="username">Ingrese un Nombre de Usuario</label>
             <input type="text" name="username" id="username" minlength="3" maxlength="15" required>
         </div>
+        <?php 
+            if(!empty($_GET['username']) && $username == 'novalid'){
+                echo '<div class="alert error">Nombre de Usuario no valido, pruebe con menos de 15 caracteres</div>';
+            }
+        ?>
         <div class="form__group">
             <label for="fullname">Ingrese su Nombre Completo</label>
             <input type="text" name="fullname" id="fullname" minlength="3" maxlength="50" required>
         </div>
+        <?php 
+            if(!empty($_GET['fullname']) && $fullname == 'novalid'){
+                echo '<div class="alert error">Su nombre no es válido en nuestro sistema</div>';
+            }
+        ?>
         <div class="form__group">
             <label for="phone">Su Teléfono</label>
             <input type="tel" name="phone" id="phone">
         </div>
+        <?php 
+            if(!empty($_GET['phone']) && $phone == 'novalid'){
+                echo '<div class="alert error">Teléfono no valido</div>';
+            }
+        ?>
         <div class="form__group">
             <label for="email">Ingrese su Correo</label>
             <input type="email" name="email" id="email" required>
         </div>
+        <?php 
+            if(!empty($_GET['email']) && $email == 'novalid'){
+                echo '<div class="alert error">Correo no valido</div>';
+            }
+        ?>
         <div class="form__group">
             <label for="password">Ingrese una Contraseña</label>
             <input type="password" name="password" id="password">
